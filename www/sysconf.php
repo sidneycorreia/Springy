65640615771330e8370641f27a62f4619268d0d7
Sidney Correia <sidneycorreia@gmail.com>
2021-08-15 22:58:26 -0300

    configuracao host producao

--- www/sysconf.php
+++ www/sysconf.php
@@ -20,7 +20,7 @@ $GLOBALS['SYSTEM'] = [
     'ENVIRONMENT_ALIAS'    => [
         'localhost'             => 'development',
         '127\.0\.0\.1'          => 'development',
-        '(www\.)?mydomain\.com' => 'production',
+        'ec2-18-222-237-201.us-east-2.compute.amazonaws.com' => 'production',
     ],
 
     // Web server doc root directory
